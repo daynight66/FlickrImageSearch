4a3120804e8acd608e3e6734268c00d80b1351e5
Chen Wang <w.chen9962@gmail.com>
2025-02-07 08:23:47 -0700

    Initial Commit

---
+++ FlickrImageSearch/FlickrImageSearchApp.swift
@@ -0,0 +1,15 @@
+//
+// FlickrImageSearchApp.swift
+// Copyright (c) 2025. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickrImageSearchUITests/FlickrImageSearchUITestsLaunchTests.swift
@@ -0,0 +1,30 @@
+//
+// FlickrImageSearchUITestsLaunchTests.swift
+// Copyright (c) 2025. All rights reserved.
+//
+
+import XCTest
+
+final class FlickrImageSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
